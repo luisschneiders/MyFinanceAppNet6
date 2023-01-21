fd2bb5d420d594b4939ab2c685027ce42b4bebef
Luis Schneiders <schneiders.tech@gmail.com>
2023-01-22 09:21:54 +1100

    setup wwwroot folder; setup folder for js and css files

---
+++ MainApp/wwwroot/js/app-color-mode.js
@@ -0,0 +1,42 @@
+﻿
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2022 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
+
+(() => {
+    'use strict'
+
+    const storedTheme = localStorage.getItem('app-theme')
+
+    const getPreferredTheme = () => {
+        if (storedTheme) {
+            return storedTheme
+        }
+
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
+
+    const setTheme = function (theme) {
+        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.setAttribute('data-bs-theme', 'dark')
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', theme)
+        }
+    }
+
+    setTheme(getPreferredTheme())
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+        if (storedTheme !== 'light' || storedTheme !== 'dark') {
+            setTheme(getPreferredTheme())
+        }
+    })
+
+    window.updateColorMode = (theme) => {
+        localStorage.setItem('app-theme', theme);
+        setTheme(theme);
+    };
+
+})()
