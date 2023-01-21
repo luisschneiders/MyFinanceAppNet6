4cdebc13596c46a142332cfe9264aa8feae4d85e
Luis Schneiders <schneiders.tech@gmail.com>
2023-01-22 09:14:29 +1100

    add helper for AuthenticationStateProvider

---
+++ MainApp/Helpers/AuthenticationStateProviderHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Components.Authorization;
+using MyFinanceAppLibrary.DataAccess;
+
+namespace MainApp.Helpers;
+
+public static class AuthenticationStateProviderHelper
+{
+    public static async Task<UserModel> GetUserFromAuth(this AuthenticationStateProvider provider, IUserData userData)
+    {
+        var authState = await provider.GetAuthenticationStateAsync();
+        string? objectId = authState.User.Claims.FirstOrDefault(c => c.Type.Contains("objectidentifier"))?.Value;
+
+        return await userData.GetUserFromAuthentication(objectId!);
+    }
+}
