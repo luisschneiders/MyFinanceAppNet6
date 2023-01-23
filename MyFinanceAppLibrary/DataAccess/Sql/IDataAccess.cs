0a58817c9506673df83c7035b1b44e07f343cb5c
Luis Schneiders <schneiders.tech@gmail.com>
2023-01-23 11:54:54 +1100

    add interface and data access for Mysql

---
+++ MyFinanceAppLibrary/DataAccess/Sql/IDataAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace MyFinanceAppLibrary.DataAccess.Sql;
+
+public interface IDataAccess
+{
+    Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName);
+    Task SaveData<T>(string storedProcedure, T parameters, string connectionStringName);
+}
---
+++ MyFinanceAppLibrary/DataAccess/Sql/MysqlDataAccess.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace MyFinanceAppLibrary.DataAccess.Sql;
+
+public class MysqlDataAccess : IDataAccess
+{
+
+#nullable disable
+    private readonly IConfiguration _config;
+#nullable enable
+
+    public MysqlDataAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName);
+
+        using (IDbConnection connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+
+            var rows = await connection.QueryAsync<T>(
+                storedProcedure,
+                parameters,
+                commandType: CommandType.StoredProcedure);
+
+            return rows.ToList();
+        };
+    }
+
+    public async Task SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName);
+
+        using (IDbConnection connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+
+            await connection.ExecuteAsync(
+                    storedProcedure,
+                    parameters,
+                    commandType: CommandType.StoredProcedure);
+        };
+    }
+}
