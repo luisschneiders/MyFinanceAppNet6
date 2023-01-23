b2809d976de3447fd2df3918205a4a56c90b4a11
Luis Schneiders <schneiders.tech@gmail.com>
2023-01-24 08:39:42 +1100

    update new property IsArchived to model Bank

--- MyFinanceAppLibrary/Models/BankModel.cs
+++ MyFinanceAppLibrary/Models/BankModel.cs
@@ -9,6 +9,7 @@ public class BankModel
     public decimal InitialBalance { get; set; }
     public decimal CurrentBalance { get; set; }
     public bool IsActive { get; set; }
+    public bool IsArchived { get; set; }
     public string UpdatedBy { get; set; }
     public DateTime CreateAt { get; set; } = DateTime.Now;
     public DateTime UpdateAt { get; set; } = DateTime.Now;
