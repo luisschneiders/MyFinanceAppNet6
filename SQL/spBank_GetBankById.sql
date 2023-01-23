5ab2b6f9e1dc58296db1fe4ec1bd71b2df99641e
Luis Schneiders <schneiders.tech@gmail.com>
2023-01-24 08:21:27 +1100

    add new stored procedure spBank_GetBankById

---
+++ SQL/spBank_GetBankById.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spBank_GetBankById`(IN userId varchar(28), bankId int)
+BEGIN
+SELECT Id, Description, Account, InitialBalance, CurrentBalance, IsActive
+    FROM Bank
+    WHERE UpdatedBy = userId
+    AND Id = bankId;
+END$$
+DELIMITER ;
