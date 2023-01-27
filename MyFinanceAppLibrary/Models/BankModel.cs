0bd4347a614eecd3d0048e19829d3478fe801831
Luis Schneiders <schneiders.tech@gmail.com>
2023-01-27 17:58:41 +1100

    add annotation Required for model Bank

--- MyFinanceAppLibrary/Models/BankModel.cs
+++ MyFinanceAppLibrary/Models/BankModel.cs
@@ -1,12 +1,24 @@
-﻿namespace MyFinanceAppLibrary.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFinanceAppLibrary.Models;
 
 public class BankModel : BaseModel
 {
 #nullable disable
     public ulong Id { get; set; }
-    public string Account { get; set; }
+
+    [Required]
     public string Description { get; set; }
+
+    [Required]
+    public string Account { get; set; }
+
+    [Required]
+    [RegularExpression(@"^\d+(.\d{1,2})?$", ErrorMessage = "Invalid value")]
     public decimal InitialBalance { get; set; }
+
+    [Required]
+    [RegularExpression(@"^\d+(.\d{1,2})?$", ErrorMessage = "Invalid value")]
     public decimal CurrentBalance { get; set; }
 #nullable enable
 }
