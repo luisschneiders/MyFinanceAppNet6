b22e67c2c961f81b5134eb34da490832b22d92e9
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-01 07:59:52 +1100

    create a generic base DA interface

---
+++ MyFinanceAppLibrary/DataAccess/Sql/IBaseData.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFinanceAppLibrary.DataAccess.Sql;
+
+public interface IBaseData<T>
+{
+    Task<List<T>> GetRecords(string userId);
+    Task<List<T>> GetSearchResults(string userId, string search);
+    Task<T> GetRecordById(string userId, string modelId);
+    Task<ulong> GetLastInsertedId();
+    Task CreateRecord(T model);
+    Task UpdateRecord(T model);
+    Task UpdateRecordStatus(T model);
+    Task ArchiveRecord(T model);
+}
