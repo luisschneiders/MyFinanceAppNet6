e372e2cf625a92f06b6536196bd3fc942cb326b8
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-01 15:52:09 +1100

    add new component and page for Transaction type

---
+++ MainApp/Pages/SetupPage/TransactionType/SetupTransactionRightPanel.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MainApp.Pages.SetupPage.TransactionType;
+
+public partial class SetupTransactionRightPanel : ComponentBase
+{
+    public SetupTransactionRightPanel()
+    {
+    }
+}
---
+++ MainApp/Pages/SetupPage/TransactionType/SetupTransactionType.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MainApp.Pages.SetupPage.TransactionType;
+
+public partial class SetupTransactionType : ComponentBase
+{
+    public SetupTransactionType()
+    {
+    }
+}
