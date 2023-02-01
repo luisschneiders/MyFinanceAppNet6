70819bc24d682bad7a102d97e5527797c989b02e
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-01 11:41:06 +1100

    add model TransactionType

---
+++ MyFinanceAppLibrary/Models/TransactionTypeModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFinanceAppLibrary.Models;
+
+public class TransactionTypeModel : BaseModel
+{
+#nullable disable
+    public ulong Id { get; set; }
+
+    [Required]
+    public string ActionType { get; set; }
+
+    [Required]
+    public string Description { get; set; }
+#nullable enable    
+}
