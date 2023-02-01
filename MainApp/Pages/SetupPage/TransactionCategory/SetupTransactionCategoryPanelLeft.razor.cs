1de3e477c52f6b61dee9e10f7b60410a8aaa6e31
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-02 09:02:14 +1100

    rename Panel components

---
+++ MainApp/Pages/SetupPage/TransactionCategory/SetupTransactionCategoryPanelLeft.razor.cs
@@ -0,0 +1,182 @@
+﻿using MainApp.Components.Spinner;
+using MainApp.Components.Toast;
+using MainApp.Pages.SetupPage.Expense;
+using Microsoft.AspNetCore.Components;
+
+namespace MainApp.Pages.SetupPage.TransactionCategory;
+
+public partial class SetupTransactionCategoryPanelLeft : ComponentBase
+{
+    [Inject]
+    ITransactionCategoryService _transactionCategoryService { get; set; } = default!;
+
+    [Inject]
+    private ToastService _toastService { get; set; } = new();
+
+    [Inject]
+    private SpinnerService _spinnerService { get; set; } = new();
+
+    /*
+     * Add OffCanvas component reference
+     */
+    private SetupTransactionCategoryOffCanvas _setupOffCanvas { get; set; } = new();
+    private TransactionCategoryModel _expenseModel { get; set; } = new();
+
+    private List<TransactionCategoryModel> _transactionCategories { get; set; } = new();
+    private List<TransactionCategoryModel> _searchResults { get; set; } = new();
+    private string _searchTerm { get; set; } = string.Empty;
+    private bool _isSearching { get; set; } = false;
+    private bool _isLoading { get; set; } = true;
+    private bool _searchButtonEnabled { get; set; } = false;
+
+    public SetupTransactionCategoryPanelLeft()
+    {
+    }
+
+    protected async override Task OnInitializedAsync()
+    {
+        await FetchDataAsync();
+        await Task.CompletedTask;
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await Task.Run(() => _spinnerService.ShowSpinner());
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private async Task SearchTermAsync(ChangeEventArgs eventArgs)
+    {
+        var searchTerm = eventArgs?.Value?.ToString();
+
+        if (string.IsNullOrWhiteSpace(searchTerm))
+        {
+            _searchResults = new();
+            _isSearching = false;
+            _searchButtonEnabled = false;
+        }
+        else
+        {
+            _searchButtonEnabled = true;
+        }
+        await Task.CompletedTask;
+    }
+
+    private async Task SearchAsync()
+    {
+        try
+        {
+            if (!string.IsNullOrWhiteSpace(_searchTerm))
+            {
+                _isSearching = true;
+                _searchResults = await _transactionCategoryService.GetSearchResults(_searchTerm);
+                StateHasChanged();
+            }
+        }
+        catch (Exception ex)
+        {
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private async Task FetchDataAsync()
+    {
+        try
+        {
+            _transactionCategories = await _transactionCategoryService.GetTransactionCategories();
+            _isLoading = false;
+        }
+        catch (Exception ex)
+        {
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private async Task UpdateStatusAsync(TransactionCategoryModel transactionCategoryModel)
+    {
+        try
+        {
+            await _transactionCategoryService.UpdateTransactionCategoryStatus(transactionCategoryModel);
+        }
+        catch (Exception ex)
+        {
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+        await Task.CompletedTask;
+    }
+
+    private async Task RefreshList()
+    {
+        // TODO: add service to refresh the list
+        //TransactionCategoryModel updatedModel = _setupOffCanvas.DataModel;
+        //TransactionCategoryModel model = _transactionCategories.FirstOrDefault(b => b.Id == updatedModel.Id)!;
+
+        //var index = _transactionCategories.IndexOf(model);
+
+        //if (index != -1)
+        //{
+        //    if (updatedModel.IsArchived)
+        //    {
+        //        var archivedModel = _transactionCategories[index] = updatedModel;
+        //        _transactionCategories.Remove(archivedModel);
+        //    }
+        //    else
+        //    {
+        //        _transactionCategories[index] = updatedModel;
+        //    }
+        //}
+        //else
+        //{
+        //    _transactionCategories.Add(updatedModel);
+        //}
+
+        //await InvokeAsync(StateHasChanged);
+        await Task.CompletedTask;
+    }
+
+    private async Task AddRecordAsync()
+    {
+        //await _setupOffCanvas.AddRecordOffCanvasAsync();
+        await Task.CompletedTask;
+    }
+
+    private async Task EditRecordAsync(TransactionCategoryModel transactionCategoryModel)
+    {
+        try
+        {
+            //await _setupOffCanvas.EditRecordOffCanvasAsync(transactionCategoryModel.Id.ToString());
+        }
+        catch (Exception ex)
+        {
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+        await Task.CompletedTask;
+    }
+
+    private async Task ViewRecordAsync(TransactionCategoryModel transactionCategoryModel)
+    {
+        try
+        {
+            //await _setupOffCanvas.ViewRecordOffCanvasAsync(transactionCategoryModel.Id.ToString());
+        }
+        catch (Exception ex)
+        {
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+        await Task.CompletedTask;
+    }
+}
