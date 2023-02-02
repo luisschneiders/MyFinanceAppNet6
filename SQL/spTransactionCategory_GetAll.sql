8a1d87195b583c141a5a643d8d51dd873c526009
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-02 11:00:26 +1100

    create Mysql stored procedures for Transaction Category

---
+++ SQL/spTransactionCategory_GetAll.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spTransactionCategory_GetAll`(
+	IN userId varchar(28)
+)
+BEGIN
+	SELECT Id, Description, ActionType, IsActive
+		FROM TransactionCategory
+		WHERE UpdatedBy = userId
+		AND IsArchived = FALSE;
+END$$
+DELIMITER ;
