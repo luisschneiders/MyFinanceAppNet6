c98faead0e914a945b6a6a4c7d5f89bdde49fe13
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-03 08:53:54 +1100

    add new components and page for Vehicle

---
+++ MainApp/Pages/SetupPage/Vehicle/SetupVehicleOffCanvas.razor.cs
@@ -0,0 +1,172 @@
+﻿using MainApp.Components.OffCanvas;
+using MainApp.Components.Toast;
+using Microsoft.AspNetCore.Components;
+using MyFinanceAppLibrary.Models;
+
+namespace MainApp.Pages.SetupPage.Vehicle;
+
+public partial class SetupVehicleOffCanvas : ComponentBase
+{
+    [Inject]
+    private IVehicleService<VehicleModel> _vehicleService { get; set; } = default!;
+
+    [Inject]
+    private IOffCanvasService _offCanvasService { get; set; } = default!;
+
+    [Inject]
+    private ToastService _toastService { get; set; } = default!;
+
+    [Parameter]
+    public EventCallback OnSubmitSuccess { get; set; }
+
+    [Parameter]
+    public VehicleModel DataModel { get; set; } = default!;
+
+    private bool _displayErrorMessages { get; set; } = false;
+    private bool _isProcessing { get; set; } = false;
+
+    private VehicleModel _vehicleModel { get; set; } = new();
+
+    public SetupVehicleOffCanvas()
+    {
+    }
+
+    public async Task AddRecordOffCanvasAsync()
+    {
+        _vehicleModel = new();
+
+        await _offCanvasService.AddRecordAsync();
+        await Task.CompletedTask;
+    }
+
+    public async Task EditRecordOffCanvasAsync(string id)
+    {
+        try
+        {
+            _vehicleModel = await _vehicleService.GetRecordById(id);
+            if (_vehicleModel is not null)
+            {
+                await _offCanvasService.EditRecordAsync(id);
+            }
+            else
+            {
+                _vehicleModel = new();
+                await Task.Delay((int)Delay.DataError);
+                _toastService.ShowToast("No record found!", Theme.Danger);
+            }
+        }
+        catch (Exception ex)
+        {
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+        await Task.CompletedTask;
+    }
+
+    public async Task ViewRecordOffCanvasAsync(string id)
+    {
+        try
+        {
+            _vehicleModel = await _vehicleService.GetRecordById(id);
+            if (_vehicleModel is not null)
+            {
+                await _offCanvasService.ViewRecordAsync(id);
+            }
+            else
+            {
+                _vehicleModel = new();
+                await Task.Delay((int)Delay.DataError);
+                _toastService.ShowToast("No record found!", Theme.Danger);
+            }
+        }
+        catch (Exception ex)
+        {
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+        await Task.CompletedTask;
+    }
+
+    private async Task UpdateFormStateAsync()
+    {
+        await _offCanvasService.UpdateFormStateAsync();
+        await Task.CompletedTask;
+    }
+
+    private async Task ArchiveAsync()
+    {
+        try
+        {
+            await _offCanvasService.ArchiveRecordAsync();
+        }
+        catch (Exception ex)
+        {
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private async Task HandleValidSubmitAsync()
+    {
+        try
+        {
+            _displayErrorMessages = false;
+            _isProcessing = true;
+
+            var offCanvasViewType = _offCanvasService.GetOffCanvasViewType();
+
+            if (offCanvasViewType == OffCanvasViewType.Add)
+            {
+                await _vehicleService.CreateRecord(_vehicleModel);
+
+                _vehicleModel.Id = await _vehicleService.GetLastInsertedId();
+                _toastService.ShowToast("Vehicle added!", Theme.Success);
+            }
+            else if (offCanvasViewType == OffCanvasViewType.Edit)
+            {
+                await _vehicleService.UpdateRecord(_vehicleModel);
+                _toastService.ShowToast("Vehicle updated!", Theme.Success);
+            }
+            else if (offCanvasViewType == OffCanvasViewType.Archive)
+            {
+                await _vehicleService.ArchiveRecord(_vehicleModel);
+                _toastService.ShowToast("Vehicle archived!", Theme.Success);
+            }
+            _isProcessing = false;
+
+            DataModel = _vehicleModel;
+
+            await OnSubmitSuccess.InvokeAsync();
+
+            await Task.Delay((int)Delay.DataSuccess);
+
+            await CloseOffCanvasAsync();
+
+        }
+        catch (Exception ex)
+        {
+            _isProcessing = false;
+
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+        await Task.CompletedTask;
+    }
+
+    private async Task HandleInvalidSubmitAsync()
+    {
+        _isProcessing = false;
+        _displayErrorMessages = true;
+        await Task.CompletedTask;
+    }
+
+    private async Task CloseOffCanvasAsync()
+    {
+        _vehicleModel = new();
+
+        await _offCanvasService.CloseAsync();
+        await Task.CompletedTask;
+    }
+}
---
+++ MainApp/Pages/SetupPage/Vehicle/SetupVehiclePanelLeft.razor.cs
@@ -0,0 +1,182 @@
+﻿using MainApp.Components.Spinner;
+using MainApp.Components.Toast;
+using MainApp.Pages.SetupPage.Vehicle;
+using Microsoft.AspNetCore.Components;
+
+namespace MainApp.Pages.SetupPage.Vehicle;
+
+public partial class SetupVehiclePanelLeft : ComponentBase
+{
+    [Inject]
+    IVehicleService<VehicleModel> _vehicleService { get; set; } = default!;
+
+    [Inject]
+    private ToastService _toastService { get; set; } = new();
+
+    [Inject]
+    private SpinnerService _spinnerService { get; set; } = new();
+
+    /*
+     * Add OffCanvas component reference
+     */
+    private SetupVehicleOffCanvas _setupOffCanvas { get; set; } = new();
+    private VehicleModel _vehicleModel { get; set; } = new();
+
+    private List<VehicleModel> _vehicles { get; set; } = new();
+    private List<VehicleModel> _searchResults { get; set; } = new();
+    private string _searchTerm { get; set; } = string.Empty;
+    private bool _isSearching { get; set; } = false;
+    private bool _isLoading { get; set; } = true;
+    private bool _searchButtonEnabled { get; set; } = false;
+
+    public SetupVehiclePanelLeft()
+    {
+    }
+    protected async override Task OnInitializedAsync()
+    {
+        await FetchDataAsync();
+        await Task.CompletedTask;
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await Task.Run(() => _spinnerService.ShowSpinner());
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private async Task SearchTermAsync(ChangeEventArgs eventArgs)
+    {
+        var searchTerm = eventArgs?.Value?.ToString();
+
+        if (string.IsNullOrWhiteSpace(searchTerm))
+        {
+            _searchResults = new();
+            _isSearching = false;
+            _searchButtonEnabled = false;
+        }
+        else
+        {
+            _searchButtonEnabled = true;
+        }
+        await Task.CompletedTask;
+    }
+
+    private async Task SearchAsync()
+    {
+        try
+        {
+            if (!string.IsNullOrWhiteSpace(_searchTerm))
+            {
+                _isSearching = true;
+                _searchResults = await _vehicleService.GetSearchResults(_searchTerm);
+                StateHasChanged();
+            }
+        }
+        catch (Exception ex)
+        {
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private async Task FetchDataAsync()
+    {
+        try
+        {
+            _vehicles = await _vehicleService.GetRecords();
+            _isLoading = false;
+        }
+        catch (Exception ex)
+        {
+            _isLoading = false;
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private async Task UpdateStatusAsync(VehicleModel vehicleModel)
+    {
+        try
+        {
+            await _vehicleService.UpdateRecordStatus(vehicleModel);
+        }
+        catch (Exception ex)
+        {
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+        await Task.CompletedTask;
+    }
+
+    private async Task RefreshList()
+    {
+        // TODO: add service to refresh the list
+        VehicleModel updatedModel = _setupOffCanvas.DataModel;
+        VehicleModel model = _vehicles.FirstOrDefault(b => b.Id == updatedModel.Id)!;
+
+        var index = _vehicles.IndexOf(model);
+
+        if (index != -1)
+        {
+            if (updatedModel.IsArchived)
+            {
+                var archivedModel = _vehicles[index] = updatedModel;
+                _vehicles.Remove(archivedModel);
+            }
+            else
+            {
+                _vehicles[index] = updatedModel;
+            }
+        }
+        else
+        {
+            _vehicles.Add(updatedModel);
+        }
+
+        await InvokeAsync(StateHasChanged);
+        await Task.CompletedTask;
+    }
+
+    private async Task AddRecordAsync()
+    {
+        await _setupOffCanvas.AddRecordOffCanvasAsync();
+        await Task.CompletedTask;
+    }
+
+    private async Task EditRecordAsync(VehicleModel vehicleModel)
+    {
+        try
+        {
+            await _setupOffCanvas.EditRecordOffCanvasAsync(vehicleModel.Id.ToString());
+        }
+        catch (Exception ex)
+        {
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+        await Task.CompletedTask;
+    }
+
+    private async Task ViewRecordAsync(VehicleModel vehicleModel)
+    {
+        try
+        {
+            await _setupOffCanvas.ViewRecordOffCanvasAsync(vehicleModel.Id.ToString());
+        }
+        catch (Exception ex)
+        {
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+        await Task.CompletedTask;
+    }
+}
