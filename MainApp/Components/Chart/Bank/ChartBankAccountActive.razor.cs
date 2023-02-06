e18e948dd35890d9b8cb777cb0903df256a1d0e0
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-07 07:35:45 +1100

    add new chart to display all active bank accounts

---
+++ MainApp/Components/Chart/Bank/ChartBankAccountActive.razor.cs
@@ -0,0 +1,91 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using MainApp.Components.Toast;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MyFinanceAppLibrary.Models;
+
+namespace MainApp.Components.Chart.Bank;
+
+public partial class ChartBankAccountActive : ComponentBase
+{
+    [Inject]
+    private IChartService _chartService { get; set; } = default!;
+
+    [Inject]
+    IBankService<BankModel> _bankService { get; set; } = default!;
+
+    [Inject]
+    private ToastService _toastService { get; set; } = new();
+
+    private List<string> _chartBackgroundColors { get; set; } = new();
+    private List<string> _chartBorderColors { get; set; } = new();
+    private List<string> _chartLabels { get; set; } = new();
+    private List<string> _chartData { get; set; } = new();
+
+    private List<BankModel> _banks { get; set; } = new();
+
+    public ChartBankAccountActive()
+    {
+    }
+
+    protected async override Task OnInitializedAsync()
+    {
+        //await FetchDataAsync();
+        await Task.CompletedTask;
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await FetchDataAsync();
+        }
+        await Task.CompletedTask;
+
+    }
+
+    private async Task FetchDataAsync()
+    {
+        try
+        {
+            _banks = await _bankService.GetRecordsActive();
+
+            foreach (var bank in _banks)
+            {
+                if (bank.CurrentBalance >= 0 && bank.CurrentBalance <= 1000)
+                {
+                    _chartBackgroundColors.Add(BackgroundColor.Gray);
+                    _chartBorderColors.Add(BorderColor.Gray);
+                }
+                else if (bank.CurrentBalance >= 1000 && bank.CurrentBalance <= 20000)
+                {
+                    _chartBackgroundColors.Add(BackgroundColor.Green);
+                    _chartBorderColors.Add(BorderColor.Green);
+                }
+                else
+                {
+                    _chartBackgroundColors.Add(BackgroundColor.Blue);
+                    _chartBorderColors.Add(BorderColor.Blue);
+                }
+                _chartLabels.Add(bank.Description);
+                _chartData.Add(bank.CurrentBalance.ToString());
+            }
+
+            var chartObjectReference = await _chartService.GetChartObjectReference();
+            await _chartService.UpdateChartData(chartObjectReference, _chartData);
+        }
+        catch (Exception ex)
+        {
+            await Task.Delay((int)Delay.DataError);
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private async Task SetChartObjectReference(IJSObjectReference chartObjectReference)
+    {
+        await _chartService.UpdateChartData(chartObjectReference, _chartData);
+        await Task.CompletedTask;
+    }
+}
