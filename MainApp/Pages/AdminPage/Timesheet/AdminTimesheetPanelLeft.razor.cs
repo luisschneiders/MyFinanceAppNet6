8edc1e864aea7daace3d81ae3e99032a2103b690
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-08 10:59:00 +1100

    Add panels for AdminTimesheet (WIP)

---
+++ MainApp/Pages/AdminPage/Timesheet/AdminTimesheetPanelLeft.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MainApp.Pages.AdminPage.Timesheet;
+
+public partial class AdminTimesheetPanelLeft : ComponentBase
+{
+    public AdminTimesheetPanelLeft()
+    {
+    }
+}
---
+++ MainApp/Pages/AdminPage/Timesheet/AdminTimesheetPanelRight.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MainApp.Pages.AdminPage.Timesheet;
+
+public partial class AdminTimesheetPanelRight : ComponentBase
+{
+    private TimesheetModel _timesheetModel { get; set; } = new();
+
+    public AdminTimesheetPanelRight()
+    {
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        // testing start
+        var timein = DateTime.Parse("2023-02-08 8:00:00");
+        var timebreak = TimeSpan.Parse("00:30:00");
+        var timeout = DateTime.Parse("2023-02-08 19:30:00");
+
+        _timesheetModel.TimeIn = timein;
+        _timesheetModel.TimeBreak = timebreak;
+        _timesheetModel.TimeOut = timeout;
+        _timesheetModel.HourRate = (decimal)88.88;
+        // testing end
+
+        await Task.CompletedTask;
+    }
+}
