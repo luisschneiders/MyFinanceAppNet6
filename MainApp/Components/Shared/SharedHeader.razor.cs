c9f82ef219c66b967e8a5a073d7acb37bb57db33
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-08 10:57:10 +1100

    Add new component SharedHeader

---
+++ MainApp/Components/Shared/SharedHeader.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MainApp.Components.Shared;
+
+public partial class SharedHeader : ComponentBase
+{
+    [Parameter]
+    public string Title { get; set; } = string.Empty;
+
+    [Parameter]
+    public string Icon { get; set; } = string.Empty;
+
+    [Parameter]
+    public Theme IconColor { get; set; } = Theme.Primary;
+
+    [Parameter]
+    public Theme BadgeBackgroundColor { get; set; } = Theme.Primary;
+
+    [Parameter]
+    public string BadgeTitle { get; set; } = string.Empty;
+
+    public SharedHeader()
+    {
+    }
+}
