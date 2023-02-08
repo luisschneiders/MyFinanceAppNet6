83284af24c915107b289e7ccf315d01c43270595
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-09 07:54:57 +1100

    Create new library DateTimeLibrary

---
+++ DateTimeLibrary/DateTimeRange.cs
@@ -0,0 +1,16 @@
+﻿namespace DateTimeLibrary;
+
+public class DateTimeRange : IDateTimeRange
+{
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+
+    public DateTimeRange()
+	{
+	}
+
+    public bool CheckDate()
+    {
+        return Start.Date <= End.Date;
+    }
+}
---
+++ DateTimeLibrary/IDateTimeRange.cs
@@ -0,0 +1,9 @@
+﻿namespace DateTimeLibrary;
+
+public interface IDateTimeRange
+{
+	public DateTime Start { get; set; }
+	public DateTime End { get; set; }
+
+	public bool CheckDate();
+}
