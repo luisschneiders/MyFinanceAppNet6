bcc8da739c8317cd3efa4198ceeedac767cd5b22
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-16 09:28:13 +1100

    update GlobalUsings; move TransactionActionType to library; add Microsoft.AspNetCore.Components.DataAnnotations.Validation version 3.2.0-rc1.20223.4

--- MainApp/GlobalUsings.cs
+++ MainApp/GlobalUsings.cs
@@ -1,4 +1,5 @@
-﻿global using MyFinanceAppLibrary.Models;
+﻿global using MyFinanceAppLibrary.Enum;
+global using MyFinanceAppLibrary.Models;
 global using MyFinanceAppLibrary.Constants;
 global using MyFinanceAppLibrary.DataAccess.NoSql;
 global using MyFinanceAppLibrary.DataAccess.Sql;
