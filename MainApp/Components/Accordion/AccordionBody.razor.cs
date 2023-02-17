d2bd5a0e37269d2e8ba6b6364d98d18ccbfb8b25
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-17 12:48:00 +1100

    Add new component Accordion

---
+++ MainApp/Components/Accordion/AccordionBody.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MainApp.Components.Accordion;
+
+public partial class AccordionBody : ComponentBase
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public string Id { get; set; } = string.Empty;
+
+    public AccordionBody()
+    {
+    }
+}
---
+++ MainApp/Components/Accordion/AccordionHeader.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MainApp.Components.Accordion;
+
+public partial class AccordionHeader : ComponentBase
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public string Id { get; set; } = string.Empty;
+
+    public AccordionHeader()
+    {
+    }
+}
