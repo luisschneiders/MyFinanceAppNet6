4fb91d890bbb8c219125feaa16efe30c3db3a1da
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-21 11:16:13 +1100

    fix bug with Stored Procedure for table Transaction when archiving a transfer movement

--- SQL/spTransaction_ArchiveTransfer.sql
+++ SQL/spTransaction_ArchiveTransfer.sql
@@ -1,9 +1,7 @@
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `spTransaction_ArchiveTransfer`(
     IN transactionId int,
-    IN transactionFromBank int,
-    IN transactionLink int,
-    IN transactionAmount decimal(10,2),
+    IN transactionIsActive bool,
     IN transactionIsArchived bool,
     IN transactionUpdatedBy varchar(28),
     IN transactionUpdatedAt datetime
@@ -13,51 +11,90 @@ BEGIN
     DECLARE rowCountTransactionDebit int default 0;
 	DECLARE rowCountBankCredit int default 0;
     DECLARE rowCountBankDebit int default 0;
-    DECLARE fromBank int default 0;
+    DECLARE varId int default 0;
+    DECLARE varLink int default 0;
+	DECLARE varFromBankCredit int default 0;
+    DECLARE varFromBankDebit int default 0;
+    DECLARE varAmount decimal(10,2) default 0;
+	DECLARE varUpdatedBy varchar(28);
+    DECLARE varCurrentBalance decimal(10,2) default 0;
 
     START TRANSACTION;
-		/* Revert Credit Transaction */
+		SELECT 
+			Id,
+            Link,
+            FromBank,
+			Amount,
+            UpdatedBy
+		INTO varId, varLink, varFromBankCredit, varAmount, varUpdatedBy
+        FROM Transaction
+        WHERE Id = transactionId
+        AND UpdatedBy = transactionUpdatedBy;
+        
+		/* Transaction Credit*/
     	UPDATE `myfinancedb`.`Transaction`
 		SET
+			`IsActive` = transactionIsActive,
 			`IsArchived` = transactionIsArchived,
 			`UpdatedBy` = transactionUpdatedBy,
 			`UpdatedAt` = transactionUpdatedAt
 		WHERE `Id` = transactionId;
         SET rowCountTransactionCredit = ROW_COUNT();
 
+        /* Transaction Debit*/
+		UPDATE `myfinancedb`.`Transaction`
+		SET
+			`IsActive` = transactionIsActive,
+			`IsArchived` = transactionIsArchived,
+			`UpdatedBy` = transactionUpdatedBy,
+			`UpdatedAt` = transactionUpdatedAt
+		WHERE `Id` = varLink;
+		SET rowCountTransactionDebit = ROW_COUNT();
+
+        /* Bank Credit*/
         IF rowCountTransactionCredit > 0 THEN
+			SELECT
+				CurrentBalance
+			INTO varCurrentBalance
+			FROM Bank
+			WHERE Id = varFromBankCredit;
+        
 			UPDATE `myfinancedb`.`Bank`
 			SET
-				`CurrentBalance` = (bankCurrentBalance - transactionAmount),
+				`CurrentBalance` = (varCurrentBalance - varAmount),
 				`UpdatedBy` = transactionUpdatedBy,
 				`UpdatedAt` = transactionUpdatedAt
-			WHERE (`Id` = transactionFromBank);
+			WHERE (`Id` = varFromBankCredit);
             SET rowCountBankCredit = ROW_COUNT();
 		END IF;
 
-		/* Revert Debit Transaction */
-		IF rowCountBankCredit > 0 THEN
-			UPDATE `myfinancedb`.`Transaction`
-			SET
-				`IsArchived` = transactionIsArchived,
-				`UpdatedBy` = transactionUpdatedBy,
-				`UpdatedAt` = transactionUpdatedAt
-			WHERE `Id` = transactionLink;
-			SET rowCountTransactionDebit = ROW_COUNT();
-            
-            IF rowCountTransactionDebit > 0 THEN
+		/* Bank Debit*/
+		IF rowCountTransactionDebit > 0 THEN
+			SELECT 
+				Id,
+				FromBank,
+				Amount,
+				UpdatedBy
+			INTO varId, varFromBankDebit, varAmount, varUpdatedBy
+			FROM Transaction
+			WHERE Id = varLink
+			AND UpdatedBy = transactionUpdatedBy;
+
+			IF rowCountBankCredit > 0 THEN        
 				SELECT
-					FromBank
-				INTO fromBank
-				FROM Transaction
-                WHERE Id = transactionLink;
-            
+					Id,
+                    CurrentBalance
+				INTO varFromBankCredit, varCurrentBalance
+				FROM Bank
+                WHERE Id = varFromBankDebit
+				AND UpdatedBy = transactionUpdatedBy;
+                
 				UPDATE `myfinancedb`.`Bank`
 				SET
-					`CurrentBalance` = (bankCurrentBalance + transactionAmount),
+					`CurrentBalance` = (varCurrentBalance + varAmount),
 					`UpdatedBy` = transactionUpdatedBy,
 					`UpdatedAt` = transactionUpdatedAt
-				WHERE (`Id` = fromBank);
+				WHERE (`Id` = varFromBankCredit);
 				SET rowCountBankDebit = ROW_COUNT();
 			END IF;
 		END IF;
