46b518669ddc86565901c50bff6f526cbc0ec3ea
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-24 07:43:46 +1100

    Add new mysql stored procedure for table Expense

---
+++ SQL/spExpense_Archive.sql
@@ -0,0 +1,60 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spExpense_Archive`(
+	IN expenseId int,
+    IN expenseBankId int,
+    IN expenseTransactionId int,
+    IN expenseAmount decimal(10,2),
+	IN expenseIsActive bool,
+    IN expenseIsArchived bool,
+    IN expenseUpdatedBy varchar(28),
+    IN expenseUpdatedAt datetime
+)
+BEGIN
+	DECLARE rowCountTransaction int default 0;
+	DECLARE rowCountBank int default 0;
+    DECLARE rowCountExpense int default 0;
+    DECLARE varCurrentBalance decimal(10,2) default 0;
+    
+    START TRANSACTION;
+    
+	SELECT
+		CurrentBalance
+	INTO varCurrentBalance
+	FROM Bank
+	WHERE Id = expenseBankId;
+
+	UPDATE `myfinancedb`.`Bank`
+	SET
+		`CurrentBalance` = (varCurrentBalance + expenseAmount),
+		`UpdatedBy` = expenseUpdatedBy,
+		`UpdatedAt` = expenseUpdatedAt
+	WHERE (`Id` = expenseBankId);
+	SET rowCountBank = ROW_COUNT();
+    
+	UPDATE `myfinancedb`.`Transaction`
+	SET
+		`IsActive` = expenseIsActive,
+		`IsArchived` = expenseIsArchived,
+		`UpdatedBy` = expenseUpdatedBy,
+		`UpdatedAt` = expenseUpdatedAt
+	WHERE `Id` = expenseTransactionId;
+	SET rowCountTransaction = ROW_COUNT();
+        
+	UPDATE `myfinancedb`.`Expense`
+	SET
+		`IsActive` = expenseIsActive,
+		`IsArchived` = expenseIsArchived,
+		`UpdatedBy` = expenseUpdatedBy,
+		`UpdatedAt` = expenseUpdatedAt
+	WHERE `Id` = expenseId;
+	SET rowCountExpense = ROW_COUNT();    
+	
+	IF (rowCountBank > 0 AND
+		rowCountTransaction > 0 AND
+        rowCountExpense > 0) THEN
+		COMMIT;
+	ELSE
+		ROLLBACK;
+	END IF;
+END$$
+DELIMITER ;
---
+++ SQL/spExpense_GetById.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spExpense_GetById`(
+	IN userId varchar(28),
+    IN expenseId int
+)
+BEGIN
+	SELECT
+		Id,
+        BankId,
+		TransactionId,
+        Amount
+	FROM Expense
+	WHERE UpdatedBy = userId
+	AND Id = expenseId
+	AND IsArchived = FALSE;
+END$$
+DELIMITER ;
