349ac6c1e18ac88af007ab0d175cbf7be61f7ebc
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-27 11:31:31 +1100

    Create interface for StateContainer; add new TimesheetStateService

---
+++ MainApp/StateServices/TimesheetStateService.cs
@@ -0,0 +1,16 @@
+﻿namespace MainApp.StateServices;
+
+public class TimesheetStateService : IStateContainer<TimesheetModelStateContainerDTO>
+{
+    public TimesheetModelStateContainerDTO Value { get; set; } = default!;
+
+    public event Action? OnStateChange;
+
+    public void SetValue(TimesheetModelStateContainerDTO value)
+    {
+        Value = value;
+        NotifyStateChanged();
+    }
+
+    private void NotifyStateChanged() => OnStateChange?.Invoke();
+}
