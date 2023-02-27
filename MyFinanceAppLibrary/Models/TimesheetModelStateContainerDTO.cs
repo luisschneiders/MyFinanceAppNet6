e33fe385e6929e03cf384aa0a07a8748aa596ab5
Luis Schneiders <schneiders.tech@gmail.com>
2023-02-27 11:30:37 +1100

    add new DTO for Timesheet;

---
+++ MyFinanceAppLibrary/Models/TimesheetModelStateContainerDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFinanceAppLibrary.Models;
+
+public class TimesheetModelStateContainerDTO
+{
+    public List<TimesheetModelListDTO> Timesheets { get; set; } = new();
+    public double TotalHours { get; set; }
+    public decimal TotalPaid { get; set; }
+    public decimal TotalAwaiting { get; set; }
+}
