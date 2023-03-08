35be5aa35263ef588616e85df4c463ee8c642213
Luis Schneiders <schneiders.tech@gmail.com>
2023-03-09 09:34:09 +1100

    update stored procedure, from previous commit

--- SQL/spExpense_GetRecordsSumLast3Months.sql
+++ SQL/spExpense_GetRecordsSumLast3Months.sql
@@ -5,13 +5,15 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `spExpense_GetRecordsSumLast3Months`
 BEGIN
 	SELECT
 		SUM(Amount) as TotalAmount,
-        month(EDate) as MonthNumber
+        month(EDate) as MonthNumber,
+        year(EDate) as YearNumber
 	FROM Expense
-	WHERE (EDate >= DATE_SUB(subdate(curdate(), (day(curdate())-1)), INTERVAL 3 MONTH))
+    WHERE 
+		EDate BETWEEN DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 3 MONTH), '%Y-%m-01') 
+		AND LAST_DAY(DATE_SUB(CURDATE(), INTERVAL 1 MONTH))
 		AND UpdatedBy = userId
 		AND IsActive = TRUE
 		AND IsArchived = FALSE
-	GROUP BY month(EDate)
-    ORDER BY month(EDate) DESC;
+	GROUP BY month(EDate), year(EDate);
 END$$
 DELIMITER ;
