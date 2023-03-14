dfe623b6c56d2ea68fe00e05e6b4e2fba6de88a7
Luis Schneiders <schneiders.tech@gmail.com>
2023-03-14 12:33:21 +1100

    add new DTO, stored procedure and chart for Transactions;

---
+++ MyFinanceAppLibrary/Models/TransactionIOLast3MonthsGraphDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFinanceAppLibrary.Models;
+
+public class TransactionIOLast3MonthsGraphDTO
+{
+#nullable disable
+    public int MonthNumber { get; }
+    public int YearNumber { get; }
+    public string Label { get; }
+    public decimal TotalAmount { get; }
+#nullable enable
+}
