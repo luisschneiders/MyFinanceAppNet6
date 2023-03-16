a502e6da059f65814683033c1fc11efb20248bf0
Luis Schneiders <schneiders.tech@gmail.com>
2023-03-17 10:48:36 +1100

    Add service for DropdownFilter

---
+++ MainApp/Components/Dropdown/Filter/IDropdownFilterService.cs
@@ -0,0 +1,8 @@
+﻿namespace MainApp.Components.Dropdown.Filter;
+
+public interface IDropdownFilterService
+{
+    Task<FilterModel> SetModel(ulong id, string description);
+    Task<FilterModel> ResetModel();
+    Task<string> UpdateLabel(string description);
+}
