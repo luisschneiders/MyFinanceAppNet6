1b147cd2df7237ba0abb466ed92eaea384c775e5
Luis Schneiders <schneiders.tech@gmail.com>
2023-03-20 08:06:27 +1100

    add new component Badge Color

---
+++ MainApp/Components/Badge/Color/BadgeColor.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MainApp.Components.Badge.Color;
+
+public partial class BadgeColor : ComponentBase
+{
+    [Parameter]
+    public string RGBColor { get; set; } = string.Empty;
+
+    [Parameter]
+    public string Description { get; set; } = string.Empty;
+
+    public BadgeColor()
+    {
+    }
+}
