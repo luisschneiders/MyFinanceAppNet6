84b0b3d46306bfa0d8972cc7b1e8066680d12be7
Luis Schneiders <schneiders.tech@gmail.com>
2023-03-23 12:53:05 +1100

    add new components for Finnhub news;

---
+++ MainApp/Components/Shared/FinnhubNews.razor.cs
@@ -0,0 +1,63 @@
+﻿using MainApp.Components.Spinner;
+using MainApp.Components.Toast;
+using Microsoft.AspNetCore.Components;
+
+namespace MainApp.Components.Shared;
+
+public partial class FinnhubNews : ComponentBase
+{
+
+    [Inject]
+    private IFinnhubService _finnhubService { get; set; } = default!;
+
+    [Inject]
+    private ToastService _toastService { get; set; } = new();
+
+    [Inject]
+    private SpinnerService _spinnerService { get; set; } = new();
+
+    Response<List<FinnhubNewsModel>> _newsList { get; set; } = default!;
+
+    private bool _isLoading { get; set; } = true;
+
+    public FinnhubNews()
+    {
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            try
+            {
+                _spinnerService.ShowSpinner();
+                await FetchDataAsync();
+            }
+            catch (Exception ex)
+            {
+                _isLoading = false;
+                _toastService.ShowToast(ex.Message, Theme.Danger);
+            }
+
+            StateHasChanged();
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private async Task FetchDataAsync()
+    {
+        try
+        {
+            _newsList = await _finnhubService.GetNewsAsync();
+            _isLoading = false;
+        }
+        catch (Exception ex)
+        {
+            _isLoading = false;
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+
+        await Task.CompletedTask;
+    }
+}
