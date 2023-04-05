dc1bf29f4a20aa0c6a033f8ebe8e0c4469da67f3
Luis Schneiders <schneiders.tech@gmail.com>
2023-04-06 09:13:14 +1000

    code updates for Weather component

--- MainApp/Settings/Enum/WeatherConditionType.cs
+++ MainApp/Settings/Enum/WeatherConditionType.cs
@@ -3,12 +3,16 @@
 public enum WeatherConditionType
 {
     //TODO: replace zeros with correct value
-    Cloudy = 1006,
-    PartlyCloudy = 1003,
     Clear = 1000,
-    Rainy = 1009,
+    PartlyCloudy = 1003,
+    Cloudy = 1006,
+    Overcast = 1009,
+    Mist = 1030,
+    PatchyRainPossible = 1063,
+    Fog = 1135,
+    PatchyLightRain = 1180,
     LightRain = 1183,
-    LightOrModerateRain = 1186,
+    ModerateRainAtTimes = 1186,
     ModerateRain = 1189,
-    ModerateOrHeavyRain = 1276
+    ModerateOrHeavyRain = 1276,
 }
