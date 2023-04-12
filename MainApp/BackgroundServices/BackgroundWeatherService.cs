7d46917e62a65d3ecb7e863171e4037a5e283df4
Luis Schneiders <schneiders.tech@gmail.com>
2023-04-13 07:54:50 +1000

    replace method OnInitializedAsync with OnAfterRenderAsync; remove private method IsNight; add ILogger

--- MainApp/BackgroundServices/BackgroundWeatherService.cs
+++ MainApp/BackgroundServices/BackgroundWeatherService.cs
@@ -7,30 +7,37 @@ namespace MainApp.BackgroundServices;
 
 public class BackgroundWeatherService : BackgroundService
 {
+    private readonly ILogger<BackgroundWeatherService> _logger;
     private readonly IServiceScopeFactory<IDateTimeService> _serviceScopeFactory;
 
-    public BackgroundWeatherService(IServiceScopeFactory<IDateTimeService> serviceScopeFactory)
+    public BackgroundWeatherService(ILogger<BackgroundWeatherService> logger, IServiceScopeFactory<IDateTimeService> serviceScopeFactory)
     {
+        _logger = logger;
         _serviceScopeFactory = serviceScopeFactory;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        try
+        _logger.LogInformation("Starting {jobName}", nameof(BackgroundWeatherService));
+
+        while (!stoppingToken.IsCancellationRequested)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
                 var dateTimeService = _serviceScopeFactory.Get();
+                var currentTime = dateTimeService.GetCurrentTime();
 
-                await Task.Delay(5000, stoppingToken);
+                _logger.LogInformation("Current time is {time}", currentTime.ToString());
 
-                Console.WriteLine("LFS - current time is: " + dateTimeService.GetCurrentTime());
             }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Job {jobName} threw an exception", nameof(BackgroundWeatherService));
+            }
+
+            await Task.Delay(5000, stoppingToken);
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine("An exception occurred: " + ex.Message);
-            throw;
-        }
+
+        _logger.LogInformation("Stopping {jobName}", nameof(BackgroundWeatherService));
     }
 }
