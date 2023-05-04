464422c4a6e7cc1e90a8f2e111aff775605f4b92
luisschneiders <30361168+luisschneiders@users.noreply.github.com>
2023-05-04 12:31:31 +1000

    Update AdminExpensePanelRight.razor.cs

--- MainApp/Pages/AdminPage/Expense/AdminExpensePanelRight.razor.cs
+++ MainApp/Pages/AdminPage/Expense/AdminExpensePanelRight.razor.cs
@@ -7,6 +7,6 @@ public partial class AdminExpensePanelRight : ComponentBase
     private string _staticMapImage { get; set; } = string.Empty;
     public AdminExpensePanelRight()
     {
-        _staticMapImage = "https://maps.googleapis.com/maps/api/staticmap?markers=color:red|-37.813628,144.963058|-37.862225,144.747939|-37.974917,145.258188&key=AIzaSyAhgTY2LNVdT_Zs0inRSg0mh3eRrOl4qpA&size=400x250&scale=2&map_id=3144ac18e4b7ff64";
+        _staticMapImage = "https://maps.googleapis.com/maps/api/staticmap?markers=color:red|-37.813628,144.963058|-37.862225,144.747939|-37.974917,145.258188&key=Key&size=400x250&scale=2&map_id=MapID";
     }
 }
