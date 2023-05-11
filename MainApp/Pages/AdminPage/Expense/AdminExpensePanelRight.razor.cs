b0650df35284ee17c5098aab5c499e16a70648cf
Luis Schneiders <schneiders.tech@gmail.com>
2023-05-11 10:30:26 +1000

    implement services for component Admin Expense Right Panel

--- MainApp/Pages/AdminPage/Expense/AdminExpensePanelRight.razor.cs
+++ MainApp/Pages/AdminPage/Expense/AdminExpensePanelRight.razor.cs
@@ -1,12 +1,90 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using MainApp.Components.Chart;
+using MainApp.Components.Toast;
+using Microsoft.AspNetCore.Components;
 
 namespace MainApp.Pages.AdminPage.Expense;
 
 public partial class AdminExpensePanelRight : ComponentBase
 {
-    private string _staticMapImage { get; set; } = string.Empty;
+    [Inject]
+    private IExpenseService<ExpenseModel> _expenseService { get; set; } = default!;
+
+    [Inject]
+    private IGoogleService _googleService { get; set; } = default!;
+
+    [Inject]
+    private ToastService _toastService { get; set; } = new();
+
+    [Inject]
+    private IDateTimeService _dateTimeService { get; set; } = default!;
+
+    [Inject]
+    private IDropdownDateRangeService _dropdownDateRangeService { get; set; } = default!;
+
+    private DateTimeRange _dateTimeRange { get; set; } = new();
+    private string _dropdownLabel { get; set; } = Label.NoDateAssigned;
+
+    private string _imageURL { get; set; } = string.Empty;
+    private bool _isLoading { get; set; } = true;
+
     public AdminExpensePanelRight()
     {
-        _staticMapImage = "https://maps.googleapis.com/maps/api/staticmap?markers=color:red|-37.813628,144.963058|-37.862225,144.747939|-37.974917,145.258188&key=Key&size=400x250&scale=2&map_id=MapID";
+    }
+
+    protected async override Task OnInitializedAsync()
+    {
+        _dateTimeRange = _dateTimeService.GetCurrentMonth();
+        _dropdownLabel = await _dropdownDateRangeService.UpdateLabel(_dateTimeRange);
+        await Task.CompletedTask;
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            try
+            {
+                await FetchDataAsync();
+            }
+            catch (Exception ex)
+            {
+                _isLoading = false;
+                _toastService.ShowToast(ex.Message, Theme.Danger);
+            }
+
+            await InvokeAsync(StateHasChanged);
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private async Task FetchDataAsync()
+    {
+        try
+        {
+            GoogleMapStaticModel model = await _expenseService.GetLocationExpense(_dateTimeRange, MapMarkerColor.Orange, MapSize.Width400, MapSize.Height250);
+            Response<string> staticImage = await _googleService.GetMapStaticImage(model);
+
+            _imageURL = staticImage.Data;
+            _isLoading = false;
+        }
+        catch (Exception ex)
+        {
+            _isLoading = false;
+            _toastService.ShowToast(ex.Message, Theme.Danger);
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private async Task DropdownDateRangeRefresh(DateTimeRange dateTimeRange)
+    {
+        _dateTimeRange = dateTimeRange;
+        _dropdownLabel = await _dropdownDateRangeService.UpdateLabel(dateTimeRange);
+        _toastService.ShowToast("Date range has changed!", Theme.Info);
+
+        await FetchDataAsync();
+
+        await Task.CompletedTask;
     }
 }
