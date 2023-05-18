8a7d819ec3280094aef5acfa1ec245c509234df2
Luis Schneiders <schneiders.tech@gmail.com>
2023-05-19 09:52:07 +1000

    Update UI and content for Admin dashboard page

--- MainApp/Pages/AdminPage/Admin.razor.cs
+++ MainApp/Pages/AdminPage/Admin.razor.cs
@@ -4,7 +4,18 @@ namespace MainApp.Pages.AdminPage;
 
 public partial class Admin : ComponentBase
 {
+    [Inject]
+    private IDateTimeService _dateTimeService { get; set; } = default!;
+
+    private DateTimeRange _dateTimeRange { get; set; } = new();
+
     public Admin()
     {
+    }
+
+    protected async override Task OnInitializedAsync()
+    {
+        _dateTimeRange = _dateTimeService.GetCurrentYear();
+        await Task.CompletedTask;
     }
 }
