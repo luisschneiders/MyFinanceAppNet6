a8392c3c064b1ab6c17102ef445831ed57259c7a
Luis Schneiders <schneiders.tech@gmail.com>
2023-06-23 14:16:07 +1000

    Get expense view type from localstorage

--- MyFinanceAppLibrary/Constants/LocalStorage.cs
+++ MyFinanceAppLibrary/Constants/LocalStorage.cs
@@ -4,4 +4,5 @@ public static class LocalStorage
 {
     public const string AppTheme = "AppTheme";
     public const string AppStartOfWeek = "AppStartOfWeek";
+    public const string AppExpenseView = "AppExpenseView";
 }
