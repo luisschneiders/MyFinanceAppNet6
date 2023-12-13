d8e960588b10c24e14813cd20128e941ccee6583
Luis Schneiders <schneiders.tech@gmail.com>
2023-12-13 18:15:42 +1100

    Create DTO FilterExpense

---
+++ MyFinanceAppLibrary/Models/FilterExpenseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFinanceAppLibrary.Models;
+
+public class FilterExpenseDTO
+{
+#nullable disable
+    public ulong BankId { get; set; }
+    public ulong ECategoryId { get; set; }
+#nullable enable
+}
