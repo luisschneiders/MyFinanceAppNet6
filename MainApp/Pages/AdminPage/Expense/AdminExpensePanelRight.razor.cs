fa7426808a73f1fbd4103adf09b074114cc10422
Luis Schneiders <schneiders.tech@gmail.com>
2024-01-02 10:30:41 +1100

    Add MapLocationExpense in the page Transactions; Code refactor

--- MainApp/Pages/AdminPage/Expense/AdminExpensePanelRight.razor.cs
+++ MainApp/Pages/AdminPage/Expense/AdminExpensePanelRight.razor.cs
@@ -4,18 +4,7 @@ namespace MainApp.Pages.AdminPage.Expense;
 
 public partial class AdminExpensePanelRight : ComponentBase
 {
-    [Inject]
-    private IDateTimeService _dateTimeService { get; set; } = default!;
-
-    private DateTimeRange _dateTimeRange { get; set; } = new();
-
     public AdminExpensePanelRight()
     {
-    }
-
-    protected async override Task OnInitializedAsync()
-    {
-        _dateTimeRange = _dateTimeService.GetCurrentMonth();
-        await Task.CompletedTask;
     }
 }
--- MainApp/Pages/AdminPage/Transaction/AdminTransactionPanelRight.razor.cs
+++ MainApp/Pages/AdminPage/Transaction/AdminTransactionPanelRight.razor.cs
@@ -4,7 +4,17 @@ namespace MainApp.Pages.AdminPage.Transaction;
 
 public partial class AdminTransactionPanelRight : ComponentBase
 {
+    [Inject]
+    private IDateTimeService _dateTimeService { get; set; } = default!;
+
+    private DateTimeRange _dateTimeRange { get; set; } = new();
+
     public AdminTransactionPanelRight()
     {
+    }
+    protected async override Task OnInitializedAsync()
+    {
+        _dateTimeRange = _dateTimeService.GetCurrentMonth();
+        await Task.CompletedTask;
     }
 }
