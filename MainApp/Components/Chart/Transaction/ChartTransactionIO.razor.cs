304aabe2a9f276214e034f575511535a84f6173f
Luis Schneiders <schneiders.tech@gmail.com>
2024-01-01 14:50:58 +1100

    Remove not in use component reference

--- MainApp/Components/Chart/Transaction/ChartTransactionIO.razor.cs
+++ MainApp/Components/Chart/Transaction/ChartTransactionIO.razor.cs
@@ -1,5 +1,4 @@
-﻿using MainApp.Components.Dropdown;
-using MainApp.Components.Spinner;
+﻿using MainApp.Components.Spinner;
 using MainApp.Components.Toast;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
