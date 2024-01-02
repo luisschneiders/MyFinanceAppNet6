2cf531859eb35c7858635c5ef78e7264924b2734
Luis Schneiders <schneiders.tech@gmail.com>
2024-01-03 04:00:27 +1100

    Remove component Purchase Location from page Transaction

--- MainApp/Pages/AdminPage/Transaction/AdminTransactionPanelRight.razor.cs
+++ MainApp/Pages/AdminPage/Transaction/AdminTransactionPanelRight.razor.cs
@@ -4,17 +4,7 @@ namespace MainApp.Pages.AdminPage.Transaction;
 
 public partial class AdminTransactionPanelRight : ComponentBase
 {
-    [Inject]
-    private IDateTimeService _dateTimeService { get; set; } = default!;
-
-    private DateTimeRange _dateTimeRange { get; set; } = new();
-
     public AdminTransactionPanelRight()
     {
     }
-    protected async override Task OnInitializedAsync()
-    {
-        _dateTimeRange = _dateTimeService.GetCurrentMonth();
-        await Task.CompletedTask;
-    }
-}
+ }
