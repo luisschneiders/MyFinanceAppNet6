285bf5001241fb3b3c8d5aa0f09fa00767074072
Luis Schneiders <schneiders.tech@gmail.com>
2024-01-08 13:49:15 +1100

    Update model and DTOs

--- MyFinanceAppLibrary/Models/TripListDTO.cs
+++ MyFinanceAppLibrary/Models/TripListDTO.cs
@@ -6,6 +6,9 @@ public class TripListDTO : BaseModel
     public ulong Id { get; }
     public DateTime TDate { get; }
     public decimal Distance { get; }
+    public decimal StartOdometer { get; }
+    public decimal EndOdometer { get; }
+    public int PayStatus { get; set; }
     public string VehicleDescription { get; }
     public string VehiclePlate { get; }
 #nullable enable
--- MyFinanceAppLibrary/Models/TripModel.cs
+++ MyFinanceAppLibrary/Models/TripModel.cs
@@ -18,5 +18,12 @@ public class TripModel : BaseModel
     [RegularExpression(@"^\d+(.\d{1,2})?$", ErrorMessage = "Invalid value")]
     [Range(0.01, int.MaxValue, ErrorMessage = "The Distance must be greater than 0.00.")]
     public decimal Distance { get; set; }
+
+    public decimal StartOdometer { get; set ; } = 0;
+
+    public decimal EndOdometer { get; set ; } = 0;
+
+    [Required]
+    public int PayStatus { get; set; } = 0;
 #nullable enable
 }
