919b869e0d236b7ea90a8175ea2027b81b358604
Luis Schneiders <schneiders.tech@gmail.com>
2024-03-23 05:39:24 +1100

    Add new DTO; update Expense Data Access to retrieve Expenses grouped by month and category

---
+++ MyFinanceAppLibrary/Models/ExpenseListGroupByMonthDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFinanceAppLibrary.Models;
+
+public class ExpenseListGroupByMonthDTO
+{
+#nullable disable
+    public ulong ECategoryId { get; }
+    public string ECategoryDescription { get; }
+    public decimal TotalAmount { get; }
+    public int MonthNumber { get; }
+#nullable enable
+}
