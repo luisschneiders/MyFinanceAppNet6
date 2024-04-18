4db67fb3c0b2b33ac6d4e35727a6d99d6c3640f7
Luis Schneiders <schneiders.tech@gmail.com>
2024-04-19 09:21:16 +1000

    Add new DTO filter

---
+++ MyFinanceAppLibrary/Models/FilterExpenseByMonthDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFinanceAppLibrary.Models;
+
+public class FilterExpenseByMonthDTO
+{
+    #nullable disable
+    public DateTimeRange DateTimeRange { get; set; }
+    public ExpenseCategoryModel ExpenseCategoryModel { get; set; }
+    public bool IsDateChanged { get; set; } = false;
+    public bool IsFilterChanged { get; set; } = false;
+    #nullable enable
+}
