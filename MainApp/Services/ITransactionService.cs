6e331248dbbc765135ce4ed08ef8dad5d42e30b8
Luis Schneiders <schneiders.tech@gmail.com>
2024-05-16 08:43:05 +1000

    Update filter for transaction

--- MainApp/Services/ITransactionService.cs
+++ MainApp/Services/ITransactionService.cs
@@ -7,8 +7,6 @@ public interface ITransactionService<T> : IBaseService<T>
     Task CreateRecordTransfer(T model);
     Task<List<TransactionIOGraphByMonthDTO>> GetIOByDateRangeGroupByMonth(DateTimeRange dateTimeRange);
     Task<List<TransactionIOGraphByDayDTO>> GetIOByDateRangeGroupByDay(DateTimeRange dateTimeRange);
-    Task<List<TransactionListDTO>> GetRecordsByDateRange(DateTimeRange dateTimeRange);
-    Task<List<TransactionByCategoryGroupDTO>> GetRecordsByFilter(DateTimeRange dateTimeRange, FilterTransactionDTO filterTransactionDTO);
-    Task<List<TransactionByCategoryGroupDTO>> GetRecordsByGroupAndDateRange(DateTimeRange dateTimeRange);
+    Task<List<TransactionByCategoryGroupDTO>> GetRecordsListView(FilterTransactionDTO filter);
     Task<List<TransactionIOLast3MonthsGraphDTO>> GetRecordsLast3Months();
 }
