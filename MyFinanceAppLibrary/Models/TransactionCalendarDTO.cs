ea6d826ed7d14af2f0fd2f416ea976aa829e7c95
Luis Schneiders <schneiders.tech@gmail.com>
2024-07-10 20:29:13 +1000

    Add new DTO for Transactions using Calendar widget;

---
+++ MyFinanceAppLibrary/Models/TransactionCalendarDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFinanceAppLibrary.Models;
+
+public class TransactionCalendarDTO
+{
+#nullable disable
+    public DateTime TDate { get; set; }
+    public string TransactionCategoryDescription { get; set; }
+    public string TransactionCategoryColor { get; set; }
+    public decimal Amount { get; set; }
+#nullable enable
+}
