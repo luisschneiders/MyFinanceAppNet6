ae4afaadec7da3bc6ec77455d5fa21ea06a1b134
Luis Schneiders <schneiders.tech@gmail.com>
2024-07-16 14:43:07 +1000

    Add status description for each record in the Calendar View; Update colour for status Awaiting;

--- MyFinanceAppLibrary/Constants/StatusColor.cs
+++ MyFinanceAppLibrary/Constants/StatusColor.cs
@@ -3,5 +3,5 @@
 public static class StatusColor
 {
     public const string Paid = "25, 135, 84";
-    public const string Awaiting = "255, 193, 7";
+    public const string Awaiting = "248, 249, 250";
 }
