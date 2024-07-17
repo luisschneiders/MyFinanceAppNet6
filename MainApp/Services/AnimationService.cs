5e27f09c182d668e2d54569ae26a24a3e3acb194
Luis Schneiders <schneiders.tech@gmail.com>
2024-07-17 14:37:33 +1000

    Celebrate transaction incomes with confettis

---
+++ MainApp/Services/AnimationService.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.JSInterop;
+
+namespace MainApp.Services;
+
+public class AnimationService : IAnimationService
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public AnimationService(IJSRuntime jSRuntime)
+    {
+        _jsRuntime = jSRuntime;
+    }
+    public async Task ConfettiTransaction()
+    {
+        try
+        {
+            await _jsRuntime.InvokeVoidAsync("startAnimation");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An exception occurred: " + ex.Message);
+            throw;
+        }
+    }
+}
