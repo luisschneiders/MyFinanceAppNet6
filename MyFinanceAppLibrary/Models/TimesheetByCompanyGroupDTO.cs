e3ee58dd42420818d7cefd7f6cb203c2750c25f2
Luis Schneiders <schneiders.tech@gmail.com>
2024-10-01 19:54:34 +1000

    Update type for TotalWorkHours in TimesheetByCompanyGroupDTO

--- MyFinanceAppLibrary/Models/TimesheetByCompanyGroupDTO.cs
+++ MyFinanceAppLibrary/Models/TimesheetByCompanyGroupDTO.cs
@@ -4,7 +4,7 @@ public class TimesheetByCompanyGroupDTO
 {
 #nullable disable
     public string Description { get; set; }
-    public TimeSpan TotalWorkHours { get; set; }
+    public double TotalWorkHours { get; set; }
     public TimeSpan TotalOvertime { get; set; }
     public decimal TotalAmount { get; set; }
     public List<TimesheetListDTO> Timesheets { get; set; } = new();
