e52837240d24d201c1231ddec95c233b7fd30420
Luis Schneiders <schneiders.tech@gmail.com>
2025-06-23 22:15:02 +1000

    Fix UX when third party javascript has integrity issues;

--- MyFinanceAppLibrary/Constants/Label.cs
+++ MyFinanceAppLibrary/Constants/Label.cs
@@ -42,6 +42,7 @@ public static class Label
     public const string AppCannotBeUndone = "This action cannot be undone.";
     public const string AppNewChart = "New Chart";
     public const string AppMessageDateRangeChanged = "Date range has changed!";
+    public const string AppMessageConfettiNotDefined = "Confetti is not defined, please check console for more info.";
 
 
     // Menubar
