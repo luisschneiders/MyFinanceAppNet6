d538741054c0c2e26bf3207c963d5845a2db961c
Luis Schneiders <schneiders.tech@gmail.com>
2025-09-11 21:11:08 +1000

    Remove not in use references

--- MainApp/Components/OffCanvas/OffCanvasMenu.razor.cs
+++ MainApp/Components/OffCanvas/OffCanvasMenu.razor.cs
@@ -1,6 +1,4 @@
-﻿using MainApp.Components.Toast;
-using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace MainApp.Components.OffCanvas;
 
@@ -8,11 +6,6 @@ public partial class OffCanvasMenu : ComponentBase
 {
     [CascadingParameter(Name = "AppSettings")]
     protected AppSettings _appSettings { get; set; } = new();
-
-    [Inject]
-    private ToastService _toastService { get; set; } = new();
-
-    private IJSObjectReference _objectReference { get; set; } = default!;
 
     public OffCanvasMenu()
 	{
